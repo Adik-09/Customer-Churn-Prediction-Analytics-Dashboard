905beaf763fa26c97eebd853d2791e440d25cda9
Aditya <aditya.kasture09@gmail.com>
2026-03-22 12:43:15 +0530

    Data set fixed for power BI

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from sklearn.model_selection import train_test_split
-import pandas as pd
-
 from src.data_preprocessing import load_data, clean_data, encode_data, split_features_target
 from src.model import train_random_forest, train_xgboost, predict
 from src.evaluate import evaluate_model
@@ -25,25 +23,42 @@ print("XGBoost:", xgb_metrics)
 
 preds, probs = predict(xgb, X)
 
-df["Churn_Prediction"] = preds
+df["Predicted_Churn"] = preds
 df["Risk_Score"] = probs
 df["Risk_Category"] = df["Risk_Score"].apply(risk_category)
 
-important_cols = [
+df["Contract_Type"] = df.apply(
+    lambda x: "One Year" if x["Contract_One year"] else (
+        "Two Year" if x["Contract_Two year"] else "Month-to-Month"
+    ),
+    axis=1
+)
+
+df["Payment_Method"] = df.apply(
+    lambda x: "Electronic Check" if x["PaymentMethod_Electronic check"] else (
+        "Credit Card" if x["PaymentMethod_Credit card (automatic)"] else "Other"
+    ),
+    axis=1
+)
+
+df.rename(columns={
+    "Churn_Yes": "Actual_Churn"
+}, inplace=True)
+
+final_cols = [
     "tenure",
     "MonthlyCharges",
     "TotalCharges",
-    "Contract_One year",
-    "Contract_Two year",
-    "PaymentMethod_Electronic check",
-    "PaymentMethod_Credit card (automatic)",
-    "Churn_Yes",
-    "Churn_Prediction",
+    "Contract_Type",
+    "Payment_Method",
+    "Actual_Churn",
+    "Predicted_Churn",
     "Risk_Score",
     "Risk_Category"
 ]
 
-df_final = df[important_cols]
+df_final = df[final_cols]
+
 df_final.to_csv("churn_predictions.csv", index=False)
 
-print("Project Successfull")
+print("✅ Project Completed Successfully")
